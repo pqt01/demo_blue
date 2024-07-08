492d9344d7efbb616584fdd249c078a15658a081
phiphiphi2k1 <philonnni@gmail.com>
2024-07-08 12:51:05 +0700

    add permision

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 import 'package:get/get.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(const MyApp());
@@ -55,6 +56,7 @@ class BluetoothApp extends StatelessWidget {
 }
 
 class BluetoothController extends GetxController {
+  static const scanTimeoutSeconds = 4; // Constant for scan timeout
   RxList<ScanResult> list = <ScanResult>[].obs;
   FlutterBlue flutterBlue = FlutterBlue.instance;
   var connectedDevice = Rx<BluetoothDevice?>(null);
@@ -66,26 +68,61 @@ class BluetoothController extends GetxController {
     scanForDevices();
   }
 
-  void scanForDevices() {
+  /// Initiates scanning for Bluetooth devices.
+  void scanForDevices() async {
+    if (!await _requestBluetoothScanPermission()) {
+      print("Bluetooth scan permission is denied.");
+      return;
+    }
+
+    _startScanning();
+  }
+
+  /// Requests the user's permission to scan for Bluetooth devices.
+  Future<bool> _requestBluetoothScanPermission() async {
+    var status = await Permission.bluetoothScan.status;
+    if (!status.isGranted) {
+      status = await Permission.bluetoothScan.request();
+    }
+    return status.isGranted;
+  }
+
+  /// Starts scanning for Bluetooth devices.
+  void _startScanning() {
     list.clear();
-    flutterBlue.scan(timeout: Duration(seconds: 4)).listen((scanResult) {
-      list.add(scanResult);
-      if (scanResult.device.name == 'gDevice-beacon') {
-        connectToDevice(scanResult.device);
-      }
-    });
+    flutterBlue
+        .scan(timeout: const Duration(seconds: scanTimeoutSeconds))
+        .listen(
+      (scanResult) {
+        // Add your scanning logic here
+      },
+      onError: (err) {
+        // Handle errors related to scanning
+        print("Error scanning for devices: $err");
+      },
+    );
   }
 
+  /// Connects to a specified Bluetooth device.
   void connectToDevice(BluetoothDevice device) async {
-    await device.connect();
-    connectedDevice.value = device;
-    discoverServices();
+    try {
+      await device.connect();
+      connectedDevice.value = device;
+      discoverServices();
+    } catch (e) {
+      print("Failed to connect to device: $e");
+    }
   }
 
+  /// Discovers services offered by the connected Bluetooth device.
   void discoverServices() async {
-    if (connectedDevice.value != null) {
+    if (connectedDevice.value == null) return;
+
+    try {
       var servicesList = await connectedDevice.value!.discoverServices();
       services.assignAll(servicesList);
+    } catch (e) {
+      print("Failed to discover services: $e");
     }
   }
 }
