2f280876fa452ebfd20e31631d9b7e1ed0af678f
pqt01 <thinhpqse151077@fpt.edu.vn>
2024-07-08 16:55:30 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -18,13 +18,12 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class BluetoothApp extends StatelessWidget {
+class BluetoothApp extends GetView<BluetoothController> {
   const BluetoothApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final BluetoothController bluetoothController =
-        Get.put(BluetoothController());
+    Get.put(BluetoothController());
     return Scaffold(
       appBar: AppBar(
         title: const Text('Bluetooth App'),
@@ -33,21 +32,24 @@ class BluetoothApp extends StatelessWidget {
         return SingleChildScrollView(
             child: Column(
           children: [
-            bluetoothController.connectedDevice.value == null
-                ? const Center(child: Text('Scanning for devices...'))
+            ElevatedButton(
+                onPressed: () {
+                  controller.scanForDevices();
+                },
+                child: const Text('Scan')),
+            controller.connectedDevice.value == null
+                ? Center(child: Text(controller.message.value))
                 : ListView.builder(
-                    itemCount: bluetoothController.services.length,
+                    itemCount: controller.services.length,
                     itemBuilder: (context, index) {
                       return ListTile(
-                        title: Text(bluetoothController.services[index].uuid
-                            .toString()),
+                        title: Text(controller.services[index].uuid.toString()),
                       );
                     },
                   ),
             Column(
-                children: bluetoothController.list
-                    .map((e) => Text(e.device.name))
-                    .toList()),
+                children:
+                    controller.list.map((e) => Text(e.device.name)).toList()),
           ],
         ));
       }),
@@ -59,23 +61,23 @@ class BluetoothController extends GetxController {
   static const scanTimeoutSeconds = 4; // Constant for scan timeout
   RxList<ScanResult> list = <ScanResult>[].obs;
   FlutterBlue flutterBlue = FlutterBlue.instance;
+  RxString message = ''.obs;
   var connectedDevice = Rx<BluetoothDevice?>(null);
   var services = <BluetoothService>[].obs;
 
-  @override
-  void onInit() {
-    super.onInit();
-    scanForDevices();
+  Future<bool> checkPermission() async {
+    bool result = await _requestBluetoothScanPermission();
+    return result;
   }
 
   /// Initiates scanning for Bluetooth devices.
   void scanForDevices() async {
-    if (!await _requestBluetoothScanPermission()) {
-      print("Bluetooth scan permission is denied.");
-      return;
+    if (await checkPermission()) {
+      Get.snackbar('Thong bao', 'Da co quyen');
+      startScanning();
+    } else {
+      Get.snackbar('Thong bao', 'Chua co quyen');
     }
-
-    _startScanning();
   }
 
   /// Requests the user's permission to scan for Bluetooth devices.
@@ -88,17 +90,20 @@ class BluetoothController extends GetxController {
   }
 
   /// Starts scanning for Bluetooth devices.
-  void _startScanning() {
+  void startScanning() {
+    message.value = 'Scanning for devices...';
     list.clear();
     flutterBlue
         .scan(timeout: const Duration(seconds: scanTimeoutSeconds))
         .listen(
       (scanResult) {
-        // Add your scanning logic here
+        list.add(scanResult);
       },
       onError: (err) {
-        // Handle errors related to scanning
-        print("Error scanning for devices: $err");
+        message.value = "Error scanning for devices: $err";
+      },
+      onDone: () {
+        message.value = 'Done';
       },
     );
   }
